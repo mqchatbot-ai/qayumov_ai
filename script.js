f917676f8f6a80086a19530f95651eaf6960a205
mqchatbot-ai <profiles422@gmail.com>
2025-05-21 00:01:34 +0500

    Update script.js

--- script.js
+++ script.js
@@ -110,7 +110,7 @@ const handleFormSubmit = (e) => {
   scrollToBottom();
   setTimeout(() => {
     // Generate bot message HTML and add in the chat container
-    const botMsgHTML = `<img class="avatar" src="/chatbot/mee.jpg" /> <p class="message-text">Qaraptur!!</p>`;
+    const botMsgHTML = `<img class="avatar" src="/chatbot/mee.jpg" /> <p class="message-text">kutib turing</p>`;
     const botMsgDiv = createMessageElement(botMsgHTML, "bot-message", "loading");
     chatsContainer.appendChild(botMsgDiv);
     scrollToBottom();
